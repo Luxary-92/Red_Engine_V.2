982689a0236fc682758f48dad736c6431780d3ee
Nixon Correa <ndncorrea@gmail.com>
2024-01-15 18:57:38 +0100

    struct channel

--- Red_Engine/ModuleAnimation.h
+++ Red_Engine/ModuleAnimation.h
@@ -1,7 +1,19 @@
 #pragma once
 #include "Module.h"
+#include "Globals.h"
 #include <vector>
 #include <string>
+#include <map>
+#include "MathGeoLib/include/Math/float3.h"
+
+struct Channel
+{
+    std::string name;
+    std::map<double, float3> posKeys;
+    std::map<double, float3> rotKeys;
+    std::map<double, float3> scaleKeys;
+};
+
 
 class Animation
 {
